5e8ba63fe2012158edf559ae5a686df739ad8d24
JYL123 <li474412210@gmail.com>
2018-11-13 18:37:51 +0800

    parallel mct & add keras-rl for reference

--- uct.py
+++ uct.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import gym
 import copy
+import multiprocessing as mp
 
 # set up the env
 env = gym.make('Centipede-ram-v0')
@@ -13,7 +14,54 @@ action_value = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 
 action_times = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], default_times)
 c = 0.6 # for UCT
 
-print(env.action_space)
+# check the number of actions available
+# print(env.action_space)
+
+def run_trials(saved, i_observation, action_Q, action_times, sameple_times):
+    env.reset()
+    env.env.restore_full_state(saved)
+
+    # choose an with highest value: in the begining it is the action hasn't tried before to make sure exploration
+    action = env.action_space.sample()
+    for key in action_value:
+        if action_value[key] > action_value[action]:
+            action = key
+
+    # increase the count of the action
+    action_times[action] = action_times[action] + 1
+
+    # look ahead 100 steps for each sampled action
+    trial_steps = 100
+    trial_reward = 0
+    trial_utility = reward
+    gamma = 0.5
+    total_gamma = 1
+    # look ahead a number of steps 
+    for trial_step in range(trial_steps):
+        # dicount
+        total_gamma = total_gamma * gamma
+        # env.env.restore_state(save_state)
+        prev_observation = i_observation
+
+        # random sample actions 
+        try_action = env.action_space.sample()
+        
+        # run trials
+        curr_observation, trial_reward, done, info = env.step(try_action)
+
+        # update   
+        trial_utility = trial_utility + total_gamma * trial_reward
+        prev_observation = curr_observation
+    
+    # update action value immediately
+    action_Q[action] = action_Q[action] + trial_utility
+    # calculate UCT
+    action_value[action] = (action_Q[action]/action_times[action] + c*math.sqrt(math.log(sameple_times+1)/(action_times[action]+0.0000000001)))
+
+def parallel_run_trials(action_samples, saved, i_observation, action_Q, action_times):
+    # assign 4 processors
+    pool = mp.Pool(4)
+    [pool.apply_async(run_trials(saved, i_observation, action_Q, action_times, sameple_times)) for sameple_times in range(action_samples)]  
 
 # run game for total steps
 total = 23
@@ -27,69 +75,18 @@ for episode in range(total):
         action_value = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], default_value)
         action_times = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], default_times)
 
-        # current state
-        # no render() because visualization is not available on the cluster
-        # env.render()
-        # print(i_observation) # observation of the env, values being the pixel intensity of the env
-        # Show the observation using OpenCV
-        # cv2.imshow('obs', i_observation)
-        # cv2.waitKey(1)
+        # save current state
         saved = env.env.clone_full_state()
+        
         # evaluate actions
         # sample k number times of actions, run trials to evaluate them
         k = 30
-        # save state to be restored
+
+        # record down the avergae value for each action simulated
         action_Q = dict.fromkeys([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17], 0)
-        for i in range(k):
-            env.reset()
-            env.env.restore_full_state(saved)
-            # env.render()
-            # print("sample actions to run simulation: ")
-            action = env.action_space.sample()
-            for key in action_value:
-                if action_value[key] > action_value[action]:
-                    action = key
-            # print(action)
-            # increase the count of the action
-            action_times[action] = action_times[action] + 1
-            # look ahead 100 steps for each sampled action
-            trial_steps = 100
-            trial_reward = 0
-            trial_utility = reward
-            gamma = 0.5
-            total_gamma = 1
-            for trial_step in range(trial_steps):
-                # print("running simulation: ")
-                total_gamma = total_gamma * gamma
-                # env.env.restore_state(save_state)
-                prev_observation = i_observation
+        parallel_run_trials(k, saved, i_observation, action_Q, action_times)
 
-
-                # choose actions with best values in trials
-                # ran_action = env.action_space.sample()
-                try_action = env.action_space.sample()
-                #for key in action_value:
-                #    if action_value[key] > action_value[try_action]:
-                #        try_action = key
-
-                # run trial with the chosen action
-                # return these 4 variables after the action is taken
-                curr_observation, trial_reward, done, info = env.step(try_action)
-                # reward received: number of blocks being hit, reward = number of 0 increased
-                # m1 = np.count_nonzero(prev_observation)
-                # m2 = np.count_nonzero(curr_observation)
-                # trial_reward = m2 - m1
-                trial_utility = trial_utility + total_gamma * trial_reward
-
-                prev_observation = curr_observation
-
-            action_Q[action] = action_Q[action] + trial_utility
-            # calculate UCT
-            # the value will be over-written several times, instead of being updated?
-            # action_times[action]+1 to make sure it is not 0
-            action_value[action] = (action_Q[action]/action_times[action] + c*math.sqrt(math.log(i+1)/(action_times[action]+0.0000000001)))
-            #print("updated value: ")
-            #print(action_value[action])
+        # exit from simulation of the node
         env.reset()
         env.env.restore_full_state(saved)
         # choose the best action
@@ -102,7 +99,6 @@ for episode in range(total):
         print(max_action)
         print(action_value[max_action])
 
-        #env.render()
         observation, reward, done, info = env.step(max_action)
         i_observation = observation # update observation
         utility = utility + reward
